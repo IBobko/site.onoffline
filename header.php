05df0b90b59848900362d8263149960c432321bc
Igor Bobko <limit-speed@yandex.ru>
2018-03-25 16:54:24 +0300

    Logo changed and php is introduced.

---
+++ header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="container">
+        <div class="header-wrap">
+            <div class="header-top d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    <a href="/"><img src="img/logo.png" alt=""></a>
+                </div>
+                <div class="main-menubar d-flex align-items-center">
+                    <nav class="hide">
+                        <a href="./">Главная</a>
+                        <a href="./get-started.php">Как установить</a>
+                    </nav>
+                    <div class="menu-bar"><span class="lnr lnr-menu"></span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
